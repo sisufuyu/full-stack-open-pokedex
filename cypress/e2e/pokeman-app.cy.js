5a9f6666befe61aaa52b4ea8e2d693561c5823f2
sisu.fuyu <sisu.fuyu@gmail.com>
2023-01-04 14:57:05 +0200

    add e2e test

--- cypress/e2e/pokeman-app.cy.js
+++ cypress/e2e/pokeman-app.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from main page to the page of a particular Pokemon', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.location('pathname').should('eq', '/pokemon/ivysaur')
+    cy.contains('overgrow')
+  })
 })
